9141c14d527953a0103308beab4430cf6eec1ce5
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2023-04-01 11:36:43 +0700

    add assets and template folder

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "../assets/css/style.css";
 
 const Footer = () => {
-    return (
-        <h1>Testing Footer</h1>
-    )
-}
+  return (
+    <>
+      <footer>
+        <div class="container">
+            <p>askdnfka jdfjaasdfajkdbfaskdjb sjakidjklsljdaknjd akdjsfnakjdfn</p>
+          <hr />
+          <p>
+            &#169; copyright{" "}
+            <a href="https://github.com/Deserve-040900/React-FE.git">Astute</a>{" "}
+            2023. All Right Reserved by{" "}
+            <a href="https://github.com/Deserve-040900">Ami.</a>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default Footer;
